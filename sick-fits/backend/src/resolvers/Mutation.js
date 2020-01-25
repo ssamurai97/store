8cf7a3fd7a250b2ca5c177f4c701ea0e0f20c5d0
kuchlong kuchlong <kuchlong@gmail.com>
2020-01-25 13:45:38 -0600

    init commit

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,31 @@
-const mutations = {};
+const Mutations = {
+    async createItem(parent, args, ctx,info){
+    //todo: check if they are logged in
+    const item = ctx.db.mutation.createItem({
+        data: {
+            ...args
 
-module.exports = mutations;
+        }
+    }, info)
+
+    console.log(item)
+    return item;
+    } ,
+     updateItem(parent, args, ctx, info){
+        //first take the a copy of the updates
+        const updates = {...args};
+        //remve Id from updates
+        delete updates.id;
+        // run the update method
+        return ctx.db.mutation.updateItem(
+            {
+                data:updates,
+                where:{
+                    id:args.id,
+                }
+            },
+            info
+        );
+   },
+}
+module.exports = Mutations;
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,18 @@
-const Query = {};
+//if query is exactly the same as both prisma and query 
+//import forwardTo to your query 
+//forwarto to db
+const { forwardTo } = require('prisma-binding')
+
+const Query = {
+    items: forwardTo('db'),
+    items: forwardTo('db'),
+//    async items(parent, args, ctx, info){
+//        const items = await ctx.db.query.items();
+//        return items;
+//    },
+
+};
+
+
 
 module.exports = Query;
